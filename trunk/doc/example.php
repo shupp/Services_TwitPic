6b6a8623576a51701a8bbdedf533ead1f1a0d1b1
Bill Shupp <hostmaster@shupp.org>
2008-08-15 17:23:46 +0000

    Add setOptions to example.php

--- trunk/doc/example.php
+++ trunk/doc/example.php
@@ -1,13 +1,16 @@
 <?php
 
+// ini_set('include_path', '../:' . ini_get('include_path'));
+
 require_once 'Services/TwitPic.php';
 
 $user     = 'username';
 $pass     = 'password';
-$filename = '/home/shupp/testimage.jpg';
+$filename = '/Users/bill/Desktop/images.jpg';
 
 $twit = new Services_TwitPic($user, $pass);
 try {
+    $twit->setOptions(array('timeout' => 10));
     $result = $twit->uploadAndPost($filename, 'testing image upload');
     print_r($result);
 } catch (Services_TwitPic_Exception $e) {
