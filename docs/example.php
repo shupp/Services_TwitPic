f3e0812641aef47591b26275027b918b3334f6b6
Bill Shupp <hostmaster@shupp.org>
2008-08-16 06:41:34 +0000

    Re-added example

---
+++ docs/example.php
@@ -0,0 +1,21 @@
+<?php
+
+// ini_set('include_path', '../:' . ini_get('include_path'));
+
+require_once 'Services/TwitPic.php';
+
+$user     = 'username';
+$pass     = 'password';
+$filename = '/Users/bill/Desktop/images.jpg';
+
+$twit = new Services_TwitPic($user, $pass);
+try {
+    $twit->setOptions(array('timeout' => 10));
+    $result = $twit->uploadAndPost($filename, 'testing image upload');
+    print_r($result);
+} catch (Services_TwitPic_Exception $e) {
+    print_r($e->getMessage());
+    print_r($e->getCode());
+}
+
+?>
