f5e3a85e4455728e42fd23f88f683fde93344404
Bill Shupp <hostmaster@shupp.org>
2008-08-16 05:48:33 +0000

    Document Mock requestor

--- trunk/Services/TwitPic/Request/Mock.php
+++ trunk/Services/TwitPic/Request/Mock.php
@@ -1,20 +1,89 @@
 <?php
+/**
+ * Services_TwitPic_Request_Mock 
+ * 
+ * PHP Version 5.1.0+
+ * 
+ * @uses      Services_TwitPic_Request_HTTPRequest
+ * @category  Services
+ * @package   Services_TwitPic
+ * @author    Bill Shupp <hostmaster@shupp.org> 
+ * @copyright 2008 Bill Shupp
+ * @license   New BSD License
+ * @link      http://servicestwitpic.googlecode.com
+ */
 
 require_once 'Services/TwitPic/Request/HTTPRequest.php';
 require_once 'Services/TwitPic/Exception.php';
 
+/**
+ * Services_TwitPic_Request_Mock 
+ * 
+ * Class for mocking response body, code, and surpressing sending data.
+ * 
+ * @uses      Services_TwitPic_Request_HTTPRequest
+ * @category  Services
+ * @package   Services_TwitPic
+ * @author    Bill Shupp <hostmaster@shupp.org> 
+ * @copyright 2008 Bill Shupp
+ * @license   New BSD License
+ * @link      http://servicestwitpic.googlecode.com
+ */
 class Services_TwitPic_Request_Mock extends Services_TwitPic_Request_HTTPRequest
 {
-    static public $requestResponse;
-    static public $responseCode;
+    /**
+     * responseBody
+     * 
+     * Canned response body for testing
+     * 
+     * @var string
+     * @access public
+     */
     static public $responseBody;
+
+     /**
+     * responseCode
+     * 
+     * Canned response code for testing
+     * 
+     * @var string
+     * @access public
+     */
+    static public $responseCode;
+
+    /**
+     * sendRequest 
+     * 
+     * Don't actually send anything
+     * 
+     * @access public
+     * @return void
+     */
     public function sendRequest()
     {
     }
+
+    /**
+     * getResponseCode 
+     * 
+     * Return the canned response code
+     * 
+     * @access public
+     * @return void
+     */
     public function getResponseCode()
     {
         return self::$responseCode;
     }
+
+    /**
+     * getResponseBody 
+     * 
+     * Return the canned response body
+     * 
+     * @access public
+     * @return void
+     */
     public function getResponseBody()
     {
         return self::$responseBody;
